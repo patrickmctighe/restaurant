099c7c6983ff51282f3d0db12f5feafcc09c97cc
patrickmctighe <115188826+patrickmctighe@users.noreply.github.com>
2023-06-14 11:03:57 -0400

    about-update

--- src/about.js
+++ src/about.js
@@ -36,6 +36,9 @@ tabs.setAttribute("class","tabs")
   aboutText2.setAttribute("class", "text");
   const aboutText3 = document.createElement("h3");
   aboutText3.setAttribute("class", "text");
+  const aboutText4 = document.createElement("h3");  
+  aboutText4.setAttribute("class", "text");
+
   const alex ="<a href='https://en.wikipedia.org/wiki/Alexander_Trocchi'>Alexander Trocchi</a>"
   aboutText1.innerHTML =
     " Aimée has her Master of Letters in Creative Writing from the University of Glasgow. She lives in North Carolina and is working on her second novel. She is the co-founder of <a href='https://www.the-crawfish.com/'>The Crawfish</a>.";
@@ -43,7 +46,8 @@ tabs.setAttribute("class","tabs")
     `Aimée is the grand-niece of Beat writer and poet ${alex} .`;
   aboutText3.innerHTML =
     `Her poetry has been exhibited at Reverberation in London, a project inspired by The Beatles seminal album ‘Sgt. Pepper’s Lonely Hearts Club Band’. She has exhibited her work at Flint Gallery in Norwich, theprintspace in London, and the Superette Gallery in Paris as a part of Never Turn Back, a photographic project headed by <a href='https://www.deanchalkley.com/theassembly/'>Dean Chalkley</a>. The two have collaborated on a publication titled One which focuses on the idea of subculture and is available through Antenne Books.`;
-  page.append(aboutText1, aboutText2, aboutText3);
+    aboutText4.innerHTML =  `She is represented by Stephanie Cabot at <a href= "https://www.susannalea.com/sla-author/aimee-keeble/">the Susanna Lea agency</a>.`
+  page.append(aboutText1, aboutText2, aboutText3, aboutText4);
 
   return page;
 };
